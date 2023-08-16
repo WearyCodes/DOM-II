2d551bb9acff287fd9978c659a21b55125ea3ef6
Sam Sanchez <WearyCodes@gmail.com>
2023-08-16 13:53:13 -0500

    day 1 fun bus

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 ## Git Setup
 
-* [ ] Create a forked copy of this project.
-* [ ] Clone your OWN version of the repository.
+* [x] Create a forked copy of this project.
+* [x] Clone your OWN version of the repository.
 * [ ] Implement the project on the main branch, committing changes regularly.
 * [ ] Push commits: `git push origin main`.
 
@@ -27,22 +27,22 @@ Do not **move or rename any files** in this project. The website's source files 
 * [ ] Using your [index.js file](src/index.js), create [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, add DOM elements, remove them, etc.
 
 * [ ] Here are some event types you could try to use:
-  * [ ] `mouseover`
-  * [ ] `keydown`
-  * [ ] `wheel`
+  * [x] `mouseover`
+  * [x] `keydown`
+  * [x] `mouseleave`
   * [ ] `load`
-  * [ ] `focus`
-  * [ ] `resize`
-  * [ ] `scroll`
+  * [x] `focus`
+  * [x] `resize`
+  * [x] `scroll`
   * [ ] `select`
-  * [ ] `dblclick`
+  * [x] `email`
   * [ ] `drag / drop`
 
 Note: Drag and drop is a bit more advanced than the others. It's not actually a single type of event but several types that need to work together.
 
 ### Use preventDefault
 
-* [ ] Find a usecase for preventDefault. For example, you could prevent a link from navigating when clicked, or to navigate somewhere surprising.
+* [x] Find a usecase for preventDefault. For example, you could prevent a link from navigating when clicked, or to navigate somewhere surprising.
 
 ## Submission Format
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
 import './less/index.less'
 
 // Your code goes here!
+const counterElement = document.getElementById('counter');
+
+let images = document.querySelectorAll('img')
+images.forEach(image => {
+    image.addEventListener('mouseover', (image) => {
+        (image.target.classList.add('Big'))
+    })
+})
+images.forEach(image => {
+    image.addEventListener('mouseleave', (image) => {
+        (image.target.classList.remove('Big'))
+    })
+})
+let counterValue = 0
+document.addEventListener('keydown', (evt) => {
+    if (evt.keyCode === 32) {
+        evt.preventDefault()
+        counterValue++;
+        counterElement.textContent = counterValue;
+        if (counterValue > 149){counterValue = 0}
+    }
+})
+let home = document.querySelector('.homelink')
+console.log(home)
+const targetElh = document.querySelector('header')
+const targetposh = targetElh.offsetTop;
+home.addEventListener('click', (event) => {
+    event.preventDefault()
+    window.scrollTo({
+        top: targetposh,
+        behavior: 'smooth'
+    })
+})
+const about = document.querySelector('.aboutlink')
+const targetEla = document.querySelector('#abouttext')
+const targetpoa = targetEla.offsetTop;
+about.addEventListener('click', (event) => {
+    event.preventDefault()
+    window.scrollTo({
+        top: targetpoa,
+        behavior: 'smooth'
+    })
+})
+const blog = document.querySelector('.bloglink')
+const targetelb = document.querySelector('#blog')
+const targetpob = targetelb.offsetTop;
+blog.addEventListener('click', (event) => {
+    event.preventDefault()
+    window.scrollTo({
+        top: targetpob,
+        behavior: 'smooth'
+    })
+})
+const contact = document.getElementsByClassName("contactlink")
+console.log(contact[0])
+document.addEventListener("DOMContentLoaded", function() {
+
+  
+    contact[0].addEventListener("click", function() {
+      const recipient = "wearycodes@gmail.com";
+      const subject = "Hello from your website!";
+      const body = "Write your email content here.";
+  
+      const mailtoLink = `mailto:${recipient}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+      
+      // Open the default email client with the pre-filled fields
+      window.location.href = mailtoLink;
+    });
+  });
